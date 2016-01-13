8c947d94cdb6ffec5aedac09cfd7c16aa340ab22
Naveen Agarwal <naveenagarwal287@gmail.com>
2016-01-13 15:28:17 +0530

    error field name config added

--- lib/password_reuse_policy/configuration.rb
+++ lib/password_reuse_policy/configuration.rb
@@ -7,7 +7,7 @@ module PasswordReusePolicy
     class << self
 
       def default!
-        password_attribute_name = :password
+        password_attribute_name = :encrypted_password
         number_of_passwords_cannot_be_used = 3
       end
 
@@ -16,7 +16,7 @@ module PasswordReusePolicy
       end
 
       def password_attribute_name
-        @password_attribute_name ||= :password
+        @password_attribute_name ||= :encrypted_password
       end
 
       def password_attribute_name=(name)
@@ -29,6 +29,14 @@ module PasswordReusePolicy
 
       def number_of_passwords_cannot_be_used=(number)
         @number_of_passwords_cannot_be_used = number
+      end
+
+      def error_field_name
+        @error_field_name ||= :password
+      end
+
+      def error_field_name=(name)
+        @error_field_name = name.to_sym
       end
 
     end
--- spec/configuration_spec.rb.rb
+++ spec/configuration_spec.rb.rb
@@ -7,17 +7,20 @@ describe PasswordReusePolicy::Configuration do
   end
 
   it 'should have default configuration' do
-    PasswordReusePolicy::Configuration.password_attribute_name.must_equal :password
+    PasswordReusePolicy::Configuration.password_attribute_name.must_equal :encrypted_password
     PasswordReusePolicy::Configuration.number_of_passwords_cannot_be_used.must_equal 3
+    PasswordReusePolicy::Configuration.error_field_name.must_equal :password
   end
 
   it 'should abe able to override default configuration' do
     PasswordReusePolicy::Configuration.config do |c|
       c.password_attribute_name = :pass
       c.number_of_passwords_cannot_be_used = 2
+      c.error_field_name = :error_pass
     end
 
     PasswordReusePolicy::Configuration.password_attribute_name.must_equal :pass
     PasswordReusePolicy::Configuration.number_of_passwords_cannot_be_used.must_equal 2
+    PasswordReusePolicy::Configuration.error_field_name.must_equal :error_pass
   end
 end
